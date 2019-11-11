53dae206444819105b7ed86290606ec2f2979321
Mario Lazzari <mario.lazzari@gmail.com>
2019-11-11 18:58:39 +0100

    Course done

--- validation.js
+++ validation.js
@@ -11,10 +11,11 @@ const patterns = {
   username: /^[a-z\d]{5,12}$/,
   telephone: /^\d{11}$/,
   password: /^[\w@-]{8,20}$/,
-  slug: /^[a-z\d-]{8,20}$/
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+  //             yourname @ domain   .  com          ( .uk )
 };
 
 // validate fx
-const validate = (field, regex) => {
-  field.className = regex.test(field.value) ? "valid" : "invalid";
-};
+const validate = (field, regex) =>
+  (field.className = regex.test(field.value) ? "valid" : "invalid");
